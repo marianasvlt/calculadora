345f8b6cb7563479f3563efb20475445bf9f7c80
marianasvlt <dhystie@gmail.com>
2024-11-28 13:42:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function alternarModo(){
+    document.body.classList.toggle('light-mode');
+}
+
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) =>{
+    btn.addEventListener("click", () =>{
+        if(btn.id === "="){
+            display.value = eval(display.value);
+        } else if (btn.id === "ac"){
+            display.value = "";
+        } else if (btn.id === "del"){
+            display.value = display.value.slice(0,-1);
+        } else{
+            display.value += btn.id
+        }
+    })
+})
